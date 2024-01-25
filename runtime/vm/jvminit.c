eclipse-openj9/openj9

Add a flag to return 0 if only one data point has been recorded
Currently, getProcessCpuLoad() and getSystemCpuLoad() return -1 if only
one data point has been recorded. A compatibility flag
-XX:[+/-]CpuLoadCompatibility is added to match the behaviour of the RI,
which is to return 0.

Fixes: https://github.com/eclipse-openj9/openj9/issues/13389
Related: https://github.com/eclipse/omr/pull/7189

Signed-off-by: Gengchen Tuo <gengchen.tuo@ibm.com>

runtime/vm/jvminit.c
@@ -4123,6 +4123,14 @@ processVMArgsFromFirstToLast(J9JavaVM * vm)
 		}
 	}
 
+	{
+		IDATA cpuLoadCompatibility = FIND_AND_CONSUME_VMARG(EXACT_MATCH, VMOPT_XXCPULOADCOMPATIBILITY , NULL);
+		IDATA noCpuLoadCompatibility = FIND_AND_CONSUME_VMARG(EXACT_MATCH, VMOPT_XXNOCPULOADCOMPATIBILITY , NULL);
+		if (cpuLoadCompatibility > noCpuLoadCompatibility) {
+			vm->extendedRuntimeFlags2 |= J9_EXTENDED_RUNTIME2_CPU_LOAD_COMPATIBILITY;
+		}
+	}
+
 	return JNI_OK;
 }
 

ibmruntimes/semeru-openj9

runtime/vm/jvminit.c
@@ -4074,6 +4074,14 @@ processVMArgsFromFirstToLast(J9JavaVM * vm)
 	}
 #endif /* JAVA_SPEC_VERSION >= 19 */
 
+	{
+		IDATA cpuLoadCompatibility = FIND_AND_CONSUME_VMARG(EXACT_MATCH, VMOPT_XXCPULOADCOMPATIBILITY, NULL);
+		IDATA noCpuLoadCompatibility = FIND_AND_CONSUME_VMARG(EXACT_MATCH, VMOPT_XXNOCPULOADCOMPATIBILITY, NULL);
+		if (cpuLoadCompatibility > noCpuLoadCompatibility) {
+			vm->extendedRuntimeFlags2 |= J9_EXTENDED_RUNTIME2_CPU_LOAD_COMPATIBILITY;
+		}
+	}
+
 	return JNI_OK;
 }
 